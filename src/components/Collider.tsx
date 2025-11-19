50cdb1b90e3d1777465dd4960517f0d26f217d59
Willem Helmet Pickleman <willemhelmet@gmail.com>
2025-11-19 12:55:12 -0500

    removed leva from collider

--- src/components/Collider.tsx
+++ src/components/Collider.tsx
@@ -1,25 +1,9 @@
 import { Box } from "@react-three/drei";
 import { type ThreeEvent } from "@react-three/fiber";
-// import { useControls } from "leva";
-// {"pos":{"x":0,"y":2,"z":0}}
 import { type ColliderTypes } from "../types";
 
 import { BackSide } from "three";
 export const Collider = ({ clicked }: ColliderTypes) => {
-  // const leva = useControls({
-  //   pos: {
-  //     x: 0,
-  //     y: 0,
-  //     z: 0,
-  //   },
-  //   scale: {
-  //     x: 1,
-  //     y: 1,
-  //     z: 1,
-  //   },
-  // });
-  // {"scale":{"x":11,"y":5.5,"z":2}}
-
   const handleClick = (e: ThreeEvent<MouseEvent>) => {
     clicked(e.point);
   };
